5c0633adbbd30a6808ef126e0b180105cc8c6e37
Hihiz <public@mail.ru>
2022-11-09 17:13:18 +0300

    Добавить страницы, картинки

---
+++ GameLauncher/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GameLauncher.ViewModel
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void onPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual void Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            onPropertyChanged(PropertyName);
+
+            return true;
+        }
+    }
+}
